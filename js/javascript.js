5df3b4b528069624712023ea18694ff650fb2ae9
stallroth <sonia.tallroth@gmail.com>
2023-04-17 18:27:54 -0500

    added lots of images,  added initial js

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+// sticky header
+
+$(window).on("scroll", function () {
+    if ($(window).scrollTop() > 75) {
+        $(".header-logo").addClass("active");
+        $("nav").addClass("active");
+        $(".navlinks").addClass("active");
+    } else {
+        $(".header-logo").removeClass("active");
+        $("nav").removeClass("active");
+        $(".navlinks").removeClass("active");
+    }
+});
